25cb3cee7cb96c290a103fc53a5529f9b39dee62
Ngayka <ngayka@gmail.com>
2026-06-27 10:48:59 +0300

    Add view files for vehicle form and list

--- add_car_to_product/models/fleet_vehicle_model.py
+++ add_car_to_product/models/fleet_vehicle_model.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class FleetVehicleModel(models.Model):
@@ -9,3 +10,15 @@ class FleetVehicleModel(models.Model):
     model_type = fields.Char(string="Model type")
     ovoko_car_id = fields.Char(string="Ovoko car ID")
     volume = fields.Many2one("fleet.vehicle.volume", string="Volume")
+
+    @api.constrains("model_year_from", "model_year_to")
+    def _check_model_years(self):
+        for rec in self:
+            if (
+                rec.model_year_from
+                and rec.model_year_to
+                and rec.model_year_to < rec.model_year_from
+            ):
+                raise ValidationError(
+                    "Year to must be greater than or equal to Year from."
+                )
