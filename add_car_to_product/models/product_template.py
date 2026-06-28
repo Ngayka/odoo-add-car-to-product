e590236dc723061e443a7a0c643f1579744b34fb
Ngayka <ngayka@gmail.com>
2026-06-28 21:30:08 +0300

    Add variant_autopart_name in product_template and add view logic in product_product_from_view.xml

--- add_car_to_product/models/product_template.py
+++ add_car_to_product/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductTemplate(models.Model):
@@ -22,3 +22,37 @@ class ProductProduct(models.Model):
     for_all_models = fields.Boolean(string="Підходить до всіх авто")
     oem = fields.Char(string="OEM")
     ovoko_part_id = fields.Char(string="Ovoko part ID")
+
+    variant_autopart_name = fields.Char(
+        string="Variant name",
+        compute="_compute_variant_autopart_name",
+    )
+
+    @api.depends(
+        "product_tmpl_id.name",
+        "compatible_vehicle_ids",
+        "compatible_vehicle_ids.brand_id",
+        "compatible_vehicle_ids.name",
+        "compatible_vehicle_ids.model_type",
+        "compatible_vehicle_ids.model_year_from",
+        "compatible_vehicle_ids.model_year_to",
+    )
+    def _compute_variant_autopart_name(self):
+        for product in self:
+            if product.is_autoparts and product.compatible_vehicle_ids:
+                vehicle = product.compatible_vehicle_ids[0]
+                years = ""
+                if vehicle.model_year_from and vehicle.model_year_to:
+                    years = f"{vehicle.model_year_from}-{vehicle.model_year_to}"
+
+                parts = [
+                    product.product_tmpl_id.name,
+                    vehicle.brand_id.name if vehicle.brand_id else "",
+                    vehicle.name or "",
+                    vehicle.model_type or "",
+                    years,
+                ]
+
+                product.variant_autopart_name = " ".join(part for part in parts if part)
+            else:
+                product.variant_autopart_name = ""
